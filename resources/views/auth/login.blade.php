f2a70de8b98730962c644f6e200f60331fc4080a
root <root@h2152900.stratoserver.net>
2015-11-27 23:40:13 +0100

    views/master.blade and Bootstrap css

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.master')
+
+@section('content')
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-4 col-sm-offset-4">
+      <h2>Please Login</h2>
+
+      <form method="post" action="{{ url('/auth/login') }}">
+        {!! csrf_field() !!}
+
+        <div class="form-group">
+          <label for="email">E-mail address</label>
+          <input type="email" class="form-control" id="email" placeholder="E-mail" name="email" value="{{ old('email') }}" required>
+        </div>
+
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" class="form-control" id="password" placeholder="password" name="password" required>
+        </div>
+
+        <div class="checkbox">
+            <label>
+              <input type="checkbox" name="remember"> Onthouden
+            </label>
+        </div>
+
+        <div class="form-group">
+          <button class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
+        </div>
+
+        @if (count($errors) > 0)
+          <div class="alert alert-danger">
+            <strong>Whoops!</strong> Het E-mail address of wachtwoord klopt niet.<br><br>
+            <ul>
+              @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+              @endforeach
+            </ul>
+          </div>
+        @endif
+
+      </form>
+@endsection

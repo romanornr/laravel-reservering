c6bbfc3ac8a315904dce4e4c8ba1e148f004a203
root <root@h2152900.stratoserver.net>
2015-11-27 13:41:09 +0100

    Database seed

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,40 @@
+<?php
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+//use database\seeds\UserTableSeeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        $this->call(UserTableSeeder::class);
+        
+        Model::reguard();
+    }
+}
+
+// Maak user
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'     => 'Travel',
+            'username' => 'travel',
+            'email'    => 'romanornr@gmail.com',
+            'password' => Hash::make('Travel1$11'),
+        ]);
+    }
+}

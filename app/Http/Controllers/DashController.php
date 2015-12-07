a220c053719f017b3c53071d4d7f3d614d100a92
romanornr <romanornr@gmail.com>
2015-12-08 00:45:46 +0100

    Fecth request test

--- app/Http/Controllers/DashController.php
+++ app/Http/Controllers/DashController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 use App\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\TravelrequestController;
+
 use Auth;
 use App\User;
+use App\Aanvragen;
+use DB;
 
 /**
 * 
@@ -18,6 +22,8 @@ class DashController extends Controller
 
 	public function show()
 	{
-		return view('dashboard.index');
+		$aanvragen = Aanvragen::orderBy('id', 'asc')->get();
+		return view('dashboard.index',
+			compact('aanvragen'));
 	}
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -5,7 +5,18 @@
 <div class="container-fluid">
   <div class="row">
     <div class="col-sm-4 col-sm-offset-4">
-      <h4>Admin Panel</h4>
-
+      <h3>Admin Panel</h3><br>
+      <h4>Aanvragen actief</h4>
+     	@foreach ($aanvragen as $aanvraag)
+    			<li>{{ $aanvraag->onderwerp }}</li>
+    			<li>{{ $aanvraag->klant_id }}</li>
+    			<li>{{ $aanvraag->bestemming }}</li>
+    			<li>{{ $aanvraag->aantal}}</li>
+    			<li>{{ $aanvraag->vertrek}}</li>
+    			<li>{{ $aanvraag->terug}}</li>
+    			<li>{{ $aanvraag->toelichting }}</li>
+    			<li>{{ $aanvraag->updated_at }}</li>
+    			<br>
+    	@endforeach
  
 @endsection

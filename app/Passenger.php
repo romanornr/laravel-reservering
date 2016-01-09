9769eef163e3494d0beffa997ae8e5ba56431908
romanornr <romanornr@gmail.com>
2016-01-10 00:45:04 +0100

    DataMutator test & PassengerController

---
+++ app/Passenger.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Passenger extends Model
+{
+    //
+}
---
+++ database/migrations/2016_01_02_000005_create_passenger_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatepassengerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('passenger', function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('booking_id')->unsigned();
+            $table->string('firstname', 32);
+            $table->string('lastname', 50)->nullable();
+            $table->string('gender', 5)->nullable();
+            $table->date('birthday');
+
+            $table->foreign('booking_id')->references('id')->on('booking')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('passenger');
+    }
+}
